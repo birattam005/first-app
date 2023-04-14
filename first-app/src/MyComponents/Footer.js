85d7da205b578db1f8c0f188893f8e91f92b1c7a
Birattam Rajak <birattam444@gmail.com>
2023-04-14 17:24:07 +0530

    added style to footer

--- first-app/src/MyComponents/Footer.js
+++ first-app/src/MyComponents/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
+  let footerStyle = {
+    position : "absolute",
+    top :"100vh",
+    width: "100%"
+  }
   return (
-    <div>
-      Footers works
-    </div>
+    <footer className='bg-dark text-light' style={footerStyle}>
+      <p className='text-center py-3'>Copyright &copy; MyTodosList.com</p>
+    </footer>
   )
 }
 
