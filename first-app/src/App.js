5353ed14e6ab4204073a995bbb4af19b83d77d8c
Birattam Rajak <birattam444@gmail.com>
2023-04-14 12:43:32 +0530

    created todos array object and onDlelete function

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,39 @@
 
 import './App.css';
-import Header from './MyComponents/Header';
+import Footer from './MyComponents/Footer';
+//import Header from './MyComponents/Header';
+import Todos from './MyComponents/Todos';
+
 
 function App() {
+
+  const onDelete =(todo) =>{
+    console.log("I am on Delete of todo",todo)
+  }
+
+  let todos = [
+    {
+      sno :1,
+      title : "Go to the marker",
+      desc:"You need to go the market to get this job done"
+    },
+    {
+      sno :2,
+      title : "Go to the mall",
+      desc:"You need to go the market to get this job done"
+    },
+    {
+      sno :3,
+      title : "Go to the ghat",
+      desc:"You need to go the market to get this job done"
+    }
+  ]
+
   return (
     <>
-    <Header />
+    
+    <Todos todos={todos} onDelete={onDelete}/>
+    <Footer />
     </>
   );
 } 
