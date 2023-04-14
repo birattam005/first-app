2fc144b4c28db4259366c5d5b264e9855edabb04
Birattam Rajak <birattam444@gmail.com>
2023-04-14 12:40:09 +0530

    header still not working

--- first-app/src/MyComponents/Header.js
+++ first-app/src/MyComponents/Header.js
@@ -1,16 +1,13 @@
-import React from "react";
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
-const Header = () => {
-
-    
-
+export default function Header() {
 
   return (
-    <div>
+    <div >
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -49,4 +46,4 @@ const Header = () => {
 }
 
 
-export default Header;
+//export default Header;
