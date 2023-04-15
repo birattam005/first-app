fcffa0691308dc19e4d76e5ccdaf7efd22b0bc94
Birattam Rajak <birattam444@gmail.com>
2023-04-15 13:07:25 +0530

    added form and useState() for adding todos

--- first-app/src/MyComponents/AddTodo.js
+++ first-app/src/MyComponents/AddTodo.js
@@ -1,32 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
 
 
-const AddTodo = () => {
+const AddTodo = (props) => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (e) =>{
+    e.preventDefault();
+    if(!title || !desc){
+      alert("Title and Description cannot be blank")
+    }
+    props.addTodo(title,desc);
+  }
+
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+    <div className='container my-3'>
+    <h3> Add a todo</h3>
+    <Form onSubmit={submit}>
+      <Form.Group className="mb-3" controlId="title">
+        <Form.Label>Todo Title</Form.Label>
+        <Form.Control type="text" value={title} onChange={(e) =>{setTitle(e.target.value)}}   placeholder="Enter Todo Title" />
+        
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="desc">
+        <Form.Label>Todo Description</Form.Label>
+        <Form.Control type="text" value = {desc} onChange={(e) =>{setDesc(e.target.value)}} placeholder="description" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button className='btn btn-sm' variant="success" type="submit">
+        Add Todo
       </Button>
     </Form>
+    </div>
   )
 }
 
