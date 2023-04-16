65074a965591e2ab6663ae45c2f561a800cbd37f
Birattam Rajak <birattam444@gmail.com>
2023-04-16 22:30:16 +0530

    react router

---
+++ first-app/src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is an about page
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere magni eaque molestiae? Id expedita praesentium dolores quasi consequatur, quas recusandae rerum asperiores distinctio ab, perspiciatis, aut quis ea natus! Tempora assumenda culpa officiis.</p>
+    </div>
+  )
+}
+
+export default About
